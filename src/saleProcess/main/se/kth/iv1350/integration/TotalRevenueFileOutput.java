a71d340e2e8843fc18cc6f45058874b5ab40dc4f
NataliAlhallak <132365555+NataliAlhallak@users.noreply.github.com>
2023-05-23 14:39:26 +0200

    observers

--- src/saleProcess/main/se/kth/iv1350/integration/TotalRevenueFileOutput.java
+++ src/saleProcess/main/se/kth/iv1350/integration/TotalRevenueFileOutput.java
@@ -5,40 +5,34 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-public class TotalRevenueFileOutput implements TotalRevenueObserver{
-    private PrintWriter logFile;
-    private static final String LOG_FILE_NAME = "observer-log.txt";
-    private StringBuilder logMsgBuilder = new StringBuilder();
 
-    /**
-     * Initializes the TotalRevenueFileOutput and creates the log file.
-     */
+public class TotalRevenueFileOutput implements TotalRevenueObserver {
+    private PrintWriter file;
+    private static final String LOG_FILE_NAME = "total-revenue-log.txt";
+
     public TotalRevenueFileOutput() {
         try {
-            logFile = new PrintWriter(new FileWriter(LOG_FILE_NAME, true));
+            file = new PrintWriter(new FileWriter(LOG_FILE_NAME, true));
         } catch (IOException error) {
-            System.out.println("Could not create file log");
+            System.out.println("An error occurred while creating the log file.");
         }
     }
 
     @Override
     public void updateTotalRevenue(double totalSaleAmount) {
-        logMsgBuilder.setLength(0);
-        logMsgBuilder.append("The total revenue for all sales at " + getTime() + " is = " + totalSaleAmount + " SEK");
-        logFile.println(logMsgBuilder.toString());
-        logFile.flush();
+        String logMsg = "Total revenue for all sales at " + getTime() + ": " + totalSaleAmount + " SEK";
+        saveLogToFile(logMsg);
     }
 
-    /**
-     * Returns the current time and date in the specified format.
-     *
-     * @return The formatted time and date.
-     */
+    private void saveLogToFile(String logMsg) {
+        file.println(logMsg);
+        file.flush();
+    }
+
     private String getTime() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date now = new Date();
-        return dateFormat.format(now);
+        return formatter.format(now);
     }
-
-
 }
+
--- src/saleProcess/main/se/kth/iv1350/view/TotalRevenueView.java
+++ src/saleProcess/main/se/kth/iv1350/view/TotalRevenueView.java
@@ -6,33 +6,12 @@ import saleProcess.main.se.kth.iv1350.integration.TotalRevenueObserver;
 
 public class TotalRevenueView implements TotalRevenueObserver {
 
-
-    /**
-     * Class TotalRevenueView implements the TotalRevenueObserver interface.
-     * It displays the total revenue on the user interface.
-     */
-
-
-        /**
-         * Creates a new TotalRevenueView instance.
-         */
-        public TotalRevenueView() {
-        }
-
-        @Override
-        public void updateTotalRevenue(double totalSaleAmount) {
-            System.out.println("The total revenue for all sales at " + getTime() + " is = " + totalSaleAmount + " SEK");
-        }
-
-        /**
-         * Returns the current time and date in the specified format.
-         *
-         * @return The formatted time and date.
-         */
-        private String getTime() {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            Date now = new Date();
-            return dateFormat.format(now);
-        }
+    public TotalRevenueView() {
     }
+
+    @Override
+    public void updateTotalRevenue(double totalSaleAmount) {
+        System.out.println("Total revenue for all sales: " + totalSaleAmount + " SEK");
+    }
+}
 
